5453136570ec23c4dd5ad3abf51515f8bef472c1
James Naftel <james@yourbase.io>
2020-03-11 13:20:16 -0400

    updated flag help

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	log.Info("demo-server starting ...")
-	port := flag.String("port", "3001", "Specify port to use")
+	port := flag.String("port", "3001", "Specify port to listen")
 	flag.Parse()
 
 	if envPort := os.Getenv("PORT"); len(envPort) > 0 {
