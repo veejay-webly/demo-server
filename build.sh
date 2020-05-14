aa65418d43b014d434bd2fcf58e649f81edb531b
James Naftel <james@yourbase.io>
2020-05-14 17:26:49 -0400

    Updated build.sh to correct tagging format (#11)
    * toggle
    
    * toggle

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-CGO_ENABLED=0 go build
-docker build -t demo/demo-server .
+commit=$(git rev-parse HEAD)
+GOOS=linux CGO_ENABLED=0 go build
+docker build -t jamesnaftel-demo-server:$commit .
+
+docker tag jamesnaftel-demo-server:$commit docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
+docker push docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
