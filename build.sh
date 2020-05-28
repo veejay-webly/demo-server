85617d8d7fb9ecf28a71e5319bc2787e7ac2dbf8
James Naftel <james@yourbase.io>
2020-05-28 10:58:59 -0400

    Togg2 (#84)
    * toggle
    
    * toggle

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ commit=$(git rev-parse HEAD)
 GOOS=linux CGO_ENABLED=0 go build
 docker build -t jamesnaftel-demo-server:$commit .
 
-docker tag jamesnaftel-demo-server:$commit docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
-docker push docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
+#docker tag jamesnaftel-demo-server:$commit docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
+#docker push docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
