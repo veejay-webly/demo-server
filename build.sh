b24f5f18ccb6d64baf8a09b2f9139004d66f8cdd
James Naftel <james@yourbase.io>
2020-12-22 15:36:39 -0500

    cleaned up experiments

--- build.sh
+++ build.sh
@@ -3,6 +3,3 @@
 commit=$(git rev-parse HEAD)
 GOOS=linux CGO_ENABLED=0 go build
 docker build -t jamesnaftel-demo-server:$commit .
-
-#docker tag jamesnaftel-demo-server:$commit docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
-#docker push docker-registry.yourbase.io/jamesnaftel-demo-server:$commit
